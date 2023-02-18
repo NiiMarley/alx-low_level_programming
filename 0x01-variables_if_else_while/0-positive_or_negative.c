4a86c07d22ae3226744fb1ee3122f048c1c3e259
NiiMarley <benjaminniimarleynai@gmail.com>
2023-02-18 11:28:30 -0800

    you

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ int main(void)
 	{
 		printf("\n%d is positive", n);
 	}
-	if (n == 0)
-	{
-		printf("\n%d is zero", n);
-	}
 	if (n < 0)
 	{
 		printf("\n%d is negative", n);
+	}
+	if (n == 0)
+	{
+		printf("\n%d is zero", n);
 	}
 	return (0);
 }
