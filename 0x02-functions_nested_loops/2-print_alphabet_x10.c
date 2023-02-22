c4fb3ae4aa6d63b5b40480b33f1b38d04a0697b1
NiiMarley <benjaminniimarleynai@gmail.com>
2023-02-22 13:56:01 -0800

    you

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,20 @@
 /**
  * 2-print_alphabet_x10.c
  * nii
- * */
+*/
 
 void print_alphabet_x10(void)
-{	int count = 0;
+{
 	char ch;
-	while count <= 9
-	for (ch = 'a'; ch <= 'z'; ch++)
+	int i;
+
+	for (int i = 0; i <= 9; i++)
 	{
-		putchar(ch);
-	}
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		}
 	putchar('\n');
-	return (0);
+	}
+return (0);
 }
