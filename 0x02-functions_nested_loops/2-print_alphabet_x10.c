1a7a85875a01f120b98633ee55b604c30ff0d322
NiiMarley <benjaminniimarleynai@gmail.com>
2023-02-22 13:32:02 -0800

    home

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * 2-print_alphabet_x10.c
+ * nii
+ * */
+
+void print_alphabet_x10(void)
+{	int count = 0;
+	char ch;
+	while count <= 9
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
